ee6b22e5d5291e568a2432255672300819d0c8d7
Alif <alifaziz@gmail.com>
2022-04-27 15:44:54 +0800

    Fix luno url

--- src/main/java/com/filavents/mycoinbot/controller/CryptoController.java
+++ src/main/java/com/filavents/mycoinbot/controller/CryptoController.java
@@ -8,12 +8,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("crypto")
