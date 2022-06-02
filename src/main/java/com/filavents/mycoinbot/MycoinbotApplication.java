06a7c43c31484caccdeda1eb604c2b55e7bc2ae3
Alif <alifaziz@gmail.com>
2022-06-02 21:24:06 +0800

    Add price command

--- src/main/java/com/filavents/mycoinbot/MycoinbotApplication.java
+++ src/main/java/com/filavents/mycoinbot/MycoinbotApplication.java
@@ -1,9 +1,5 @@
 package com.filavents.mycoinbot;
 
-import com.pengrad.telegrambot.TelegramBot;
-import com.pengrad.telegrambot.UpdatesListener;
-import com.pengrad.telegrambot.request.SendMessage;
-import com.pengrad.telegrambot.response.SendResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,22 +8,6 @@ public class MycoinbotApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MycoinbotApplication.class, args);
-
-        // Create your bot passing the token received from @BotFather
-        TelegramBot bot = new TelegramBot(System.getenv("TELEGRAM_BOT_TOKEN"));
-
-        // Register for updates
-        bot.setUpdatesListener(updates -> {
-            // ... process updates
-            updates.forEach(update -> {
-                System.out.println(update.message());
-                long chatId = update.message().chat().id();
-                SendResponse response = bot.execute(new SendMessage(chatId, "Hi! " + update.message().from().firstName()));
-            });
-            // return id of last processed update or confirm them all
-            return UpdatesListener.CONFIRMED_UPDATES_ALL;
-        });
-
     }
 
 }
---
+++ src/main/java/com/filavents/mycoinbot/startup/TelegramBot.java
@@ -0,0 +1,72 @@
+package com.filavents.mycoinbot.startup;
+
+import com.filavents.mycoinbot.model.Crypto;
+import com.filavents.mycoinbot.service.CryptoService;
+import com.filavents.mycoinbot.service.impl.LunoCryptoServiceImpl;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+@Component
+public class TelegramBot implements ApplicationRunner {
+
+    @Value("${mycoinbot.lunoEndpointUrl}")
+    private String lunoEndpointUrl;
+
+    @Autowired
+    private final CryptoService cryptoService = new LunoCryptoServiceImpl();
+
+    private com.pengrad.telegrambot.TelegramBot bot = null;
+
+    @Override
+    public void run(ApplicationArguments args) {
+
+        System.out.println("Starting Telegram Bot...");
+
+        // Create your bot passing the token received from @BotFather
+        bot = new com.pengrad.telegrambot.TelegramBot(System.getenv("TELEGRAM_BOT_TOKEN"));
+
+        // Register for updates
+        bot.setUpdatesListener(updates -> {
+            updates.forEach(update -> {
+                try {
+                    long chatId = update.message().chat().id();
+                    switch (update.message().text()) {
+                        case "/price":
+                            Crypto crypto = cryptoService.getLatestCryptoPrice(
+                                    "BTC",
+                                    lunoEndpointUrl
+                            );
+                            String currentPrice = "Luno - " + formatCurrency(crypto.getPrice().doubleValue());
+                            replyMessage(chatId, currentPrice);
+                        default:
+                            break;
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            });
+            // return id of last processed update or confirm them all
+            return UpdatesListener.CONFIRMED_UPDATES_ALL;
+        });
+    }
+
+    private String formatCurrency(double price) {
+        NumberFormat currency = NumberFormat.getCurrencyInstance(new Locale("ms", "MY"));
+        return currency.format(price);
+    }
+
+    private void replyMessage(long chatId, String message) {
+        SendResponse response = bot.execute(
+                new SendMessage(chatId, message)
+        );
+    }
+}
