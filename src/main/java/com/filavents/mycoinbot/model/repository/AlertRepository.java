a521e79f49f133d1f30966278dc76cc1c0210f63
Alif <alifaziz@gmail.com>
2022-06-02 22:12:42 +0800

    Add missing repository annotation

--- src/main/java/com/filavents/mycoinbot/model/repository/AlertRepository.java
+++ src/main/java/com/filavents/mycoinbot/model/repository/AlertRepository.java
@@ -2,7 +2,9 @@ package com.filavents.mycoinbot.model.repository;
 
 import com.filavents.mycoinbot.model.Alert;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AlertRepository extends JpaRepository<Alert, Long> {
 
 }
