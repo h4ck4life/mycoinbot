43a05c06c464227201c22597543d71f7a9ede1f8
Alif <alifaziz@gmail.com>
2022-06-06 20:50:47 +0800

    Remove unused imports

--- src/main/java/com/filavents/mycoinbot/controller/CryptoController.java
+++ src/main/java/com/filavents/mycoinbot/controller/CryptoController.java
@@ -13,7 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
