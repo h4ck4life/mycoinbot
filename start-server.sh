f7a8219ef8bb48d147ec410cf95598e9e47d9d4a
Alif <alifaziz@gmail.com>
2022-10-26 13:11:40 +0000

    Update server scripts

--- start-server.sh
+++ start-server.sh
@@ -1,4 +1,5 @@
+export PATH=/opt/apache-maven-3.8.6/bin:$PATH
 git pull origin master &&
 export TELEGRAM_BOT_TOKEN=5304881616:AAHO2sMJry5AZHryTxsrqND0ex3uo8H5gQ4
 mvn clean package -DskipTests &&
-mvn spring-boot:start
+mvn spring-boot:start -Dspring-boot.run.arguments='--server.port=8085'
--- stop-server.sh
+++ stop-server.sh
@@ -1,2 +1,3 @@
+export PATH=/opt/apache-maven-3.8.6/bin:$PATH
 mvn spring-boot:stop
 kill -9 `lsof -t -c java`
